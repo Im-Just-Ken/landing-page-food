6a9cd16e9a3bbd9985b44e693af3e8a5d1941b2d
TopGun <kenneth.tecson07@gmail.com>
2025-01-30 02:55:13 +0800

    final 1

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -26,7 +26,7 @@ export default defineComponent({
     <div class="w-full max-w-[1440px] mx-auto px-4 sm:px-6 flex justify-center">
       <ul class="w-full flex flex-wrap justify-center gap-6">
         <li
-          class="feature-card relative bg-cover bg-center w-[340px] sm:w-[300px] md:w-[300px] lg:w-[300px] h-76 rounded-xl p-4 shadow-lg overflow-hidden flex-col motion-opacity-in-0 motion-translate-y-in-50 motion-delay-200"
+          class="feature-card relative bg-cover bg-center w-[400px] sm:w-[300px] md:w-[300px] lg:w-[300px] h-76 rounded-xl p-4 shadow-lg overflow-hidden flex-col motion-opacity-in-0 motion-translate-y-in-50 motion-delay-200"
           :style="{ backgroundImage: `url(${feature.image})` }"
           v-for="feature in features"
           :key="feature.id"
